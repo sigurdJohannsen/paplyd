f7bf83ad72366348c897dbca4208647480a2c0af
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-02 16:10:42 +0200

    csvreader added

---
+++ Assets/Scripts/Data/SoundAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAnimation {
+
+    public string Tag;
+    public List<TimeStep> TimeStepList = new List<TimeStep>();
+
+    public class TimeStep
+    {
+        public float time;
+        public string Mund;
+        public string Ojne;
+        public string Krop;
+
+    }
+
+}
