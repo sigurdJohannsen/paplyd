9aa1918cd4a97c2095bfac7666ec86bb21516dd0
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-03 14:17:14 +0200

    Animation player added secruity + Wwise post event

--- Assets/Scripts/Data/AnimationPlayer.cs
+++ Assets/Scripts/Data/AnimationPlayer.cs
@@ -16,20 +16,39 @@ public class AnimationPlayer : MonoBehaviour {
     public IEnumerator PlayAnimation(string tag)
     {
         AnimationDatabase.Instance.SoundAnimationDictionary.TryGetValue(tag,out currentSoundAnimation);
-        //Wwise
-        //PostEvent.Play(currentSoundAnimation.Tag);
+
+        AkSoundEngine.PostEvent(tag, gameObject);
+
         timeWaited = 0.0f;
         Sprite tempMund = null, tempOjne = null, tempKrop = null;
         for (int i = 0; i < currentSoundAnimation.TimeStepList.Count; i++)
         {
             yield return new WaitForSeconds(currentSoundAnimation.TimeStepList[i].time - timeWaited);
             timeWaited += currentSoundAnimation.TimeStepList[i].time;
-            AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Mund, out tempMund);
-            AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Ojne, out tempOjne);
-            AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Krop, out tempKrop);
-            Mund.sprite = tempMund;
-            Ojne.sprite = tempOjne;
-            Krop.sprite = tempKrop;
+            if (AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Mund, out tempMund))
+            {
+                Mund.sprite = tempMund;
+            }
+            else
+            {
+                Debug.LogError(currentSoundAnimation.TimeStepList[i].Mund + " could not be found");
+            }
+            if (AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Ojne, out tempOjne))
+            {
+                Ojne.sprite = tempOjne;
+            }
+            else
+            {
+                Debug.LogError(currentSoundAnimation.TimeStepList[i].Ojne + " could not be found");
+            }
+            if (AnimationDatabase.Instance.MundList.TryGetValue(currentSoundAnimation.TimeStepList[i].Krop, out tempKrop))
+            {
+                Krop.sprite = tempKrop;
+            }
+            else
+            {
+                Debug.LogError(currentSoundAnimation.TimeStepList[i].Krop + " could not be found");
+            }
         }
         yield return true;
     }
