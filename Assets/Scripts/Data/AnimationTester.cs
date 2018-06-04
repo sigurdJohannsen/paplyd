7804e838c324e10e598f92ab1ae1df56c041bc90
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-04 10:41:54 +0200

    nu er alt med : og mit sovsetræ er sat på "Pending"

---
+++ Assets/Scripts/Data/AnimationTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTester : MonoBehaviour {
+
+    AnimationPlayer ap;
+    private void Start()
+    {
+        ap = GetComponent<AnimationPlayer>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            StartCoroutine(ap.PlayAnimation("Monkey"));  
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ap.PlayAnimation("Rooster"));
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(ap.PlayAnimation("Elephant"));
+        }
+    }
+}
