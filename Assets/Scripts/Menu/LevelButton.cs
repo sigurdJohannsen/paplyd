08393bab93d948591a0dc6f92aec210c24901568
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-04 23:03:49 +0200

    Scene System (SS) er done

---
+++ Assets/Scripts/Menu/LevelButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour {
+
+    public int Level;
+
+    public void Clicked()
+    {
+        LevelManager.Instance.LoadScene(Level);
+    }
+}
---
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using BaseClasses;
+using UnityEngine.UI;
+
+public class LevelManager : BaseSingleton<LevelManager>
+{
+
+    public GameObject LevelSelectorContent;
+
+    /// <summary>
+    /// To load the scene at number
+    /// </summary>
+    /// <param SceneNumber="i"></param>
+    public void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    public void LoadNextLevel()
+    {
+        int n = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(n + 1);
+    }
+
+
+    public void OnLevelWasLoaded(int level)
+    {
+        if(level == 1)
+        {
+            SpawnAllLevels();
+        }
+    }
+
+    public void SpawnAllLevels()
+    {
+        if (!FindObjectOfType<GridLayoutGroup>())
+        {
+            Debug.LogError("Couldn't find the GridLayoutGroup that you were looking for");
+            return;
+        }
+
+
+        Transform scrollRect = FindObjectOfType<GridLayoutGroup>().transform;
+        // Mainmenu, LevelSelecter 
+        for (int i = 2; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            GameObject levelButton = Instantiate(LevelSelectorContent, scrollRect) as GameObject;
+            levelButton.GetComponentInChildren<Text>().text = "Level " + (i-1);
+            levelButton.GetComponent<LevelButton>().Level = i;
+        }
+    }
+
+
+}
