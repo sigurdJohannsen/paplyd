2c10a029528667147a801936454c8d550a2aadcc
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-04 19:18:18 +0200

    draggable and stuff

---
+++ Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableObject : MonoBehaviour, IEndDragHandler, IPointerClickHandler, IBeginDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.localScale += Vector3.one * 1.01f;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+        private Vector3 screenPoint;
+        private Vector3 offset;
+
+        void OnMouseDown()
+        {
+
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+
+        void OnMouseDrag()
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+}
