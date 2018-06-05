d0c9844210b2b843f06a33f882f17b7593848c60
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-06-05 13:31:12 +0200

    animation and good vibrations

--- Assets/Scripts/Data/AnimationTester.cs
+++ Assets/Scripts/Data/AnimationTester.cs
@@ -3,6 +3,7 @@
 public class AnimationTester : MonoBehaviour {
 
     AnimationPlayer ap;
+    bool playing = false;
     private void Start()
     {
         ap = GetComponent<AnimationPlayer>();
@@ -10,19 +11,22 @@ public class AnimationTester : MonoBehaviour {
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.M)|| Input.touchCount > 1)
+        if(Input.GetKeyDown(KeyCode.M) && !playing)
         {
+            playing = true;
             StartCoroutine(ap.PlayAnimation("Monkey", (bool b) => AnimationCallback(b)));
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !playing)
         {
+            playing = true;
             StartCoroutine(ap.PlayAnimation("Rooster", (bool b) => AnimationCallback(b)));
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !playing)
         {
+            playing = true;
             StartCoroutine(ap.PlayAnimation("Elephant", (bool b) => AnimationCallback(b)));
         }
     }
 
-    private void AnimationCallback(bool b) { }
+    private void AnimationCallback(bool b) { playing = false; }
 }
---
+++ Assets/Scripts/Data/VibrationAnimation.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationAnimation : MonoBehaviour {
+
+
+    [Range (0,1)]
+    public float VibrationVolume;
+
+    private int animationID;
+    private float animationVibrationLevel;
+    public List<Transform> ListOfVibrationElements = new List<Transform>();
+    private List<Vector3> originalPositions = new List<Vector3>();
+    public void ResetPositions()
+    {
+        for (int i = 0; i < ListOfVibrationElements.Count; i++)
+        {
+            ListOfVibrationElements[i].localPosition = originalPositions[i];
+        }
+    }
+    public void Awake()
+    {
+        originalPositions = new List<Vector3>();
+        for (int i = 0; i < ListOfVibrationElements.Count; i++)
+        {
+            originalPositions.Add(ListOfVibrationElements[i].localPosition);
+        }
+    }
+    public void Vibrate(int animation)
+    {
+        Debug.Log(animation + " VIBRATION");
+        animationID = animation;
+        //ResetPositions();
+        switch (animation)
+        {
+            case 0:
+                animationVibrationLevel = 0f;
+                break;
+            case 1:
+                animationVibrationLevel = 1.5f;
+                break;
+            case 2:
+                animationVibrationLevel = 1.0f;
+                break;
+            case 3:
+                animationVibrationLevel = 0.5f;
+                break;
+            default:
+                break;
+        }
+    }
+    private void LateUpdate()
+    {
+        if(animationID != 0)
+        {
+            VibrateAnimation(animationVibrationLevel);
+        }
+
+    }
+    private void VibrateAnimation(float VibrateAmount)
+    {
+        Debug.Log(VibrateAmount * VibrationVolume + " vibration");
+        for (int i = 0; i < ListOfVibrationElements.Count; i++)
+        {
+            ListOfVibrationElements[i].localPosition +=  Random.insideUnitSphere * VibrateAmount * VibrationVolume;
+        }
+    }
+}
